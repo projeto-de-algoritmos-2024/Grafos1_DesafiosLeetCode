82f1724b70b0c7b9d8a2107d23d1342de84de7e0
GabrielCastelo-31 <gabflubr@outlook.com>
2024-11-09 22:59:59 -0300

    Detectar Conflitos de Cor Durante a BFS
    Co-authored-by: julio-dourado <juliodourado419@gmail.com>

--- DesafioGrafobipartido/solution2.py
+++ DesafioGrafobipartido/solution2.py
@@ -1,16 +1,22 @@
 fila = []
 
 # Enfilera
+
+
 def enfileirar(item):
     fila.append(item)
-#Desenfilera
+
+# Desenfilera
+
+
 def desenfileirar():
     if not fila:  # Se for vazia
         return None
     return fila.pop(0)
 
+
 class Solution(object):
-    def bfs_colore(self, grafo, inicio):
+    def bfs_verifica_conflitos(self, grafo, inicio):
         cor = [-1] * len(grafo)
         enfileirar(inicio)
         cor[inicio] = 0
@@ -19,9 +25,9 @@ class Solution(object):
             no = desenfileirar()
             for vizinho in grafo[no]:
                 if cor[vizinho] == -1:
-                    cor[vizinho] = 1 - cor[no]  # Atribui cor alternada
+                    cor[vizinho] = 1 - cor[no]  # Atribui cor alternad
                     enfileirar(vizinho)
+                elif cor[vizinho] == cor[no]:
+                    return False  # Conflito encontrado, não é bipartido
 
-        return cor
-    
-print(Solution().bfs_colore([[1, 3], [0, 2], [1, 3], [0, 2]], 0))
+        return True
